8ab7871f5659b349ac50de80ef6e600f4f278601
Bailey Todd <btodd914@gmail.com>
2019-01-18 13:35:20 -0600

    first commit

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Bailey Todd on 1/4/19.
+//  Copyright © 2019 Bailey Todd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
